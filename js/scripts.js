1e2edb34dcdbea0c46a93ff57a909693e8fc2968
Xavier <touchaville@gmail.com>
2017-03-23 22:32:13 +0100

    added triangleArea script. Added h1 centering style

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
-/* zmienna */
-var name = prompt('Wpisz swoje imię');
-//komunikat alert
-alert('Witaj, ' + name);
-// wiadomość w konsoli
-console.log('Witaj, ' + name);
+
+
+alert('Chcesz obliczyć pole trójkąta? Nic prostszego!   Wystarczy podać odpowiednie wartości.');
+var a = prompt('wpisz wartość podstawy trójkąta a');
+var h = prompt('wpisz wartość wysokości trójkąta h');
+var triangleArea = (a*h/2);
+
+alert( 'Pole trójkąta o podstawie a: ' +a+ ' i wysokości h: ' +h+ ' wynosi: ' + triangleArea);
+
+console.log('pole trójkąta o podstawie a:'+a+' i wysokości h: '+h+' wynosi:' + triangleArea);
