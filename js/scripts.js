4506ec43314d1d956c5414d94f8aee823a591df7
Xavier <touchaville@gmail.com>
2017-03-24 23:44:08 +0100

    first js function

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+
+function getTriangleArea(a, h) {
+    
+    if (a, h <= 0) {
+        alert('nieprawilowe dane');
+    }
+    else {
+        return (a * h / 2);
+    }
+}
+console.log(getTriangleArea(10,6)); 
+
+var triangle1Area = getTriangleArea(10,15);
+var triangle2Area = getTriangleArea(3,15);
+var triangle3Area = getTriangleArea(12,5);
+
+console.log (triangle1Area);
+console.log (triangle2Area);
+console.log (triangle3Area);
